b722c4b579ba332c90221ea0d4b916e289546d27
Marvin Ola <olamarvin40@gmail.com>
2024-05-13 22:38:02 -0600

    introduccion a listas, colas y pilas

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,21 +1,22 @@
-#La busqueda binaria es un algoritmo de busqueda eficiente que busca un item en una lista ordenada, 
-#compara el item con el valor del medio de la lista y elimina la mitad en la que el item no puede estar en cada iteracion.
+# La busqueda binaria es un algoritmo de busqueda eficiente que busca un item en una lista ordenada,
+# compara el item con el valor del medio de la lista y elimina la mitad en la que el item no puede estar en cada iteracion.
 def binary_search(list, item):
- low = 0 
- high = len(list) - 1 
+    low = 0
+    high = len(list) - 1
 
- while low <= high: 
-  mid = (low + high)
-  guess = list[mid]
-  if guess == item:
-   return mid
-  if guess > item: 
-   high = mid - 1
- else: 
-  low = mid + 1
- return None 
+    while low <= high:
+        mid = low + high
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+    else:
+        low = mid + 1
+    return None
+
 
 my_list = [5, 3, 5, 7, 9]
 
-print(binary_search(my_list, 3)) # => 1
-print(binary_search(my_list, -1)) # => None
+print(binary_search(my_list, 3))  # => 1
+print(binary_search(my_list, -1))  # => None
